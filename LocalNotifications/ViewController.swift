c584787d9ed6ff0b0e7e048aaee6936d1600c8d4
Ahmed Zaki <a.zaki@rkanjel.com>
2019-05-11 11:52:17 +0200

    Initial Commit

---
+++ LocalNotifications/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalNotifications
+//
+//  Created by RKAnjel on 5/11/19.
+//  Copyright © 2019 RKAnjel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
