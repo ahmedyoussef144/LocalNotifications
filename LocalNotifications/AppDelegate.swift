eacb18b1723c5913ab9f695635da6b7d59ff6fcf
Ahmed Zaki <a.zaki@rkanjel.com>
2019-05-11 13:31:39 +0200

    make notification show in foreground

--- LocalNotifications/AppDelegate.swift
+++ LocalNotifications/AppDelegate.swift
@@ -11,12 +11,13 @@ import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
+    
     let notificationsCenter = UNUserNotificationCenter.current()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+       notificationsCenter.delegate = self
         let options: UNAuthorizationOptions = [.alert, .sound, .badge]
         notificationsCenter.requestAuthorization(options: options) {
             (didAllow, error) in
@@ -26,37 +27,47 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         return true
     }
-    func scheduleNotifiacitons(notificationType: String ) {
-        let content = UNMutableNotificationContent()
-        content.title = "Local Notifications"
-        content.subtitle = "Notifications"
-        content.body = "this is example to learn how to create " + "\(notificationType) Notification"
-        content.sound = UNNotificationSound.default
-        content.badge = 1
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        UIApplication.shared.applicationIconBadgeNumber = 0
     }
+    
+}
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+extension AppDelegate: UNUserNotificationCenterDelegate {
+   
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        print(response.notification.request.content.userInfo)
+        completionHandler()
     }
-
-
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .badge, .sound])
+    }
+    
+    func scheduleNotification(notificationType: String) {
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Notifiaction on a certail date"
+        content.body = "This is a local notification on certain date"
+        content.sound = .default
+        content.badge = 1
+        content.userInfo = ["value": "Data with local notification"]
+        
+        
+        let fireDate = Calendar.current.dateComponents([.day, .month, .year, .hour, .minute, .second], from: Date().addingTimeInterval(10))
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: fireDate, repeats: false)
+        let request = UNNotificationRequest(identifier: "reminder", content: content, trigger: trigger)
+        notificationsCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
+        
+    }
 }
 
